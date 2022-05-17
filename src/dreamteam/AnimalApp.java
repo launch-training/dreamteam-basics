e9346aa6a482feeabdc834cb74d9bc507dc514db
Christian Grobmeier <cg@grobmeier.de>
2022-05-17 09:58:59 +0200

    added new code

--- src/dreamteam/AnimalApp.java
+++ src/dreamteam/AnimalApp.java
@@ -17,27 +17,46 @@ public class AnimalApp {
 		System.out.println("Hello " + name4);
 		System.out.println("Hello " + name5);
 	*/	
-		
-		/*
 		String[] names = new String[] { 
-				"Christian", "Florian", "Minh", "Tanja", "Deeksha" };
-		*/
+				"Christian", "Florian", "Minh", "Tanja", "Deeksha" };	
 		
-		String xyz = "asdfasdf";
-		String[] names = args;
-		int x = 0;
-		
-		// Foreach
 		for (String name : names) {
 			System.out.println("Hello " + name);
-			x = x + 1;
-			notifyOfRuns(x);
 		}
 		
-		System.out.println("This code run " + x + " times");
+		// Data Object = POJO = Entity = Business Object
+		Person p1 = new Person();
+		p1.name = "Christian";
+		p1.age = 44;
+
+		Person p2 = new Person();
+		p2.name = "Florian";
+		p2.age = 26;
 		
-		notifyOfRuns(x);
+		Person p3 = new Person();
+		p3.name = "Minh";
+		p3.age = 27;
 		
+		Person p4 = new Person();
+		p4.name = "Tanja";
+		p4.age = 26;
+		
+		Person p5 = new Person();
+		p5.name = "Deeksha";
+		p5.age = 28;
+		
+		Person[] persons = new Person[] { p1, p2, p3, p4, p5 };
+		
+		// more modern version of looping
+		for (Person temp : persons) {
+			System.out.println("Hello " + temp.name + ", age " + temp.age);
+		}
+		
+		// Oldschool but also possible:
+		for (int i = 0; i < persons.length; i++) {
+			System.out.println("Hello " + persons[i].name + ", age " + persons[i].age);
+			i++;
+		}
 	}
 	
 	public static void notifyOfRuns(int y) {
